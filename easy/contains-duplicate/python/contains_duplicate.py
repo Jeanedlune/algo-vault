7165cae7975caa820248009158ef1360212ae130
Vansh Dagar <vanshdagar2705@gmail.com>
2025-10-20 21:41:23 +0530

    feat(py): implement contains-duplicate using set

--- easy/contains-duplicate/python/contains_duplicate.py
+++ easy/contains-duplicate/python/contains_duplicate.py
@@ -1,6 +1,4 @@
 from typing import List
-
-
 def contains_duplicate(nums: List[int]) -> bool:
     seen = set()
     for n in nums:
@@ -11,7 +9,14 @@ def contains_duplicate(nums: List[int]) -> bool:
 
 
 if __name__ == "__main__":
-    tests = [[1, 2, 3, 1], [1, 2, 3, 4], [1, 1, 1, 3, 3, 4, 3, 2, 4, 2], [], [42]]
 
+    tests = [[1, 2, 3, 1], [1, 2, 3, 4], [1, 1, 1, 3, 3, 4, 3, 2, 4, 2], [], [42]]
+    tests = [
+        [1, 2, 3, 1],
+        [1, 2, 3, 4],
+        [1, 1, 1, 3, 3, 4, 3, 2, 4, 2],
+        [],
+        [42]
+    ]
     for t in tests:
         print(contains_duplicate(t))
