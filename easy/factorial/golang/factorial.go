87f3e7ff962dd0a5d9504a79e81a7de7f8192a13
0xrelogic <maedeeaew@gmail.com>
2025-11-10 13:07:40 +0000

    feat(go): implement factorial

---
+++ easy/factorial/golang/factorial.go
@@ -0,0 +1,13 @@
+package factorial
+
+// Factorial returns the factorial of n
+func Factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	return result
+}
