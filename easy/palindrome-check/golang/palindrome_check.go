45a86557cfeacd4cb08318590406cd7baf8e4a70
0xrelogic <maedeeaew@gmail.com>
2025-11-10 13:06:54 +0000

    feat(go): implement palindrome-check

---
+++ easy/palindrome-check/golang/palindrome_check.go
@@ -0,0 +1,11 @@
+package palindromecheck
+
+// IsPalindrome checks if a string reads the same forwards and backwards
+func IsPalindrome(s string) bool {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
