31b14fcf7127cda6983f16bd6cf89c7b010f1b36
Ubuntu <azureuser@rdpcoding.1pf2nbvqxbiu5avafz5ryxemth.ix.internal.cloudapp.net>
2025-11-11 19:31:36 +0000

    feat(go): implement merge-k-sorted-lists solution
    - Add O(N log k) solution using min-heap
    - Handles empty lists and edge cases
    - Follows Go heap interface conventions

---
+++ hard/merge-k-sorted-lists/golang/merge_k_sorted_lists.go
@@ -0,0 +1,41 @@
+package mergeksortedlists
+
+import (
+	"container/heap"
+)
+
+type MinHeap []int
+
+func (h MinHeap) Len() int           { return len(h) }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MinHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func MergeKSortedLists(lists [][]int) []int {
+	var result []int
+	minHeap := &MinHeap{}
+	heap.Init(minHeap)
+	
+	for _, list := range lists {
+		for _, num := range list {
+			heap.Push(minHeap, num)
+		}
+	}
+	
+	for minHeap.Len() > 0 {
+		result = append(result, heap.Pop(minHeap).(int))
+	}
+	
+	return result
+}
